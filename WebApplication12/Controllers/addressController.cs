f11306c08f2bf7bda5e4811023f3465ee78f84a1
this pc <this pc@DESKTOP-NQOAETK>
2022-03-08 15:27:19 +0530

    hhhhhAdd project files.

---
+++ WebApplication12/Controllers/addressController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication12.Controllers
+{
+    public class addressController : Controller
+    {
+        // GET: addressController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: addressController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: addressController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: addressController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: addressController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: addressController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: addressController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: addressController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
